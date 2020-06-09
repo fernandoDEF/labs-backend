13a2a0f4464b08553404b7b3d9c8481fa63de559
Diego Pastás <31971098+fernandoDEF@users.noreply.github.com>
2020-06-08 20:18:53 -0500

    Update LabsApplication.java

--- src/main/java/co/edu/unal/software_engineering/labs/LabsApplication.java
+++ src/main/java/co/edu/unal/software_engineering/labs/LabsApplication.java
@@ -9,5 +9,5 @@ public class LabsApplication{
     public static void main( String[] args ){
         SpringApplication.run( LabsApplication.class );
     }
-    //primer commit aqui
+    //commit aqui
 }
